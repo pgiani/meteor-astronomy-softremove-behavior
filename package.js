6ba38cef187d4d1df27bf48e93ef754a7d799a03
pablo Giani <pgiani@gmail.com>
2019-12-04 13:06:17 -0700

    first commit

---
+++ package.js
@@ -0,0 +1,21 @@
+Package.describe({
+  name: 'pgiani:astronomy-softremove-behavior',
+  version: '3.0.0',
+  summary: 'Soft remove behavior for Meteor Astronomy',
+  git: 'https://github.com/jagi/meteor-astronomy-softremove-behavior.git',
+});
+
+Npm.depends({
+  lodash: '4.17.11',
+});
+
+Package.onUse(function(api) {
+  api.versionsFrom('1.3');
+
+  api.use(
+    ['ecmascript', 'es5-shim', 'pgiani:astronomy@2.0.0'],
+    ['client', 'server'],
+  );
+
+  api.mainModule('lib/main.js', ['client', 'server'], { lazy: true });
+});
